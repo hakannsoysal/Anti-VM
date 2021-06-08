bf810abf1241d40ff0481670b425b4908d5dc7ff
hakannsoysal <79102823+hakannsoysal@users.noreply.github.com>
2021-06-09 01:05:29 +0300

    Add files via upload

---
+++ Anti-VM.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fileapi.h>
+#include <TlHelp32.h>
+#include <strsafe.h>
+
+int main()
+{   
+    // Check files.
+    WIN32_FIND_DATAW findFileData; // WIN32_FIND_DATAW yapısına yönelik bir işaretci.
+    HANDLE file; // API'ı tutar.
+    file = FindFirstFileW( // FindFirstFileW verdiğiniz ada uygun dosya veya alt dizin arar.
+        L"C:\\Windows\\System32\\vm3dgl.dll", // bu parametre dizinin, path'in adı olabilir.
+        &findFileData // WIN32_FIND_DATAW yapısına yönelik bir işaretci. Dizin veya dosya hakkında bilgi alır.
+    );
+
+    if (file != INVALID_HANDLE_VALUE) // kontrol
+        std::cout << "Virtual machine detected.\n";
+    else
+        std::cout << "Virtual machine not detected.\n";
+
+    Sleep(5000);
+
+    
+    // Check registry key.
+    HKEY hkResult;
+    LONG key;
+    key = RegOpenKeyExW( // RegOpenKeyExW API'ı belirtilen kayıt defteri keyini açar.
+        HKEY_LOCAL_MACHINE, // Key handle'ı. Arayacağımız yer burası.
+        L"System\\ControlSet001\\Services\\VMTools", // Path
+        0, // Anahtar açılırken uygulanacak seçenek.
+        KEY_QUERY_VALUE, // Açılacak keyin istenen erişim hakkı.
+        &hkResult // Açılan keye bir pointer.
+    );
+    
+    if (key == ERROR_SUCCESS)
+        std::cout << "Virtual machine detected.\n";
+    else
+        std::cout << "Virtual machine not detected.\n";
+        
+    Sleep(5000);
+    
+    
+
+    // Check computer name.
+    BOOL pcName;
+    const int buffer_size = MAX_COMPUTERNAME_LENGTH + 1;
+    wchar_t lpBuffer[buffer_size];
+    DWORD nSize;
+    nSize = buffer_size;
+    pcName = GetComputerNameW(
+        lpBuffer,
+        &nSize
+    );
+
+    if (pcName == FALSE) {
+        throw std::runtime_error("Something went wrong.");
+    }
+    wprintf(L"Computer Name: %s\n", lpBuffer);
+    Sleep(3000);
+
+    if (wcsstr(lpBuffer, L"WIN-"))
+        std::cout << "Virtual machine detected.";
+    Sleep(5000);
+
+
+
+    // Check window name.
+    LPCWSTR windowName = L"x32dbg";
+    if (FindWindow(NULL, windowName)) { // iki parametre alıyor. ilki null olabilir, null olursa tüm pencere adlarını alır. ikinci parametremiz pencere ismi
+        MessageBoxA(NULL, "Debugger detected.", "Notification", MB_OK);
+    }
+    else
+        MessageBoxA(NULL, "Debugger not detected.", "Notification", MB_OK);
+    
+
+    Sleep(5000);
+
+    // Are you a robot ?
+    int response = MessageBoxW(NULL, L"Bilgisayarınızı şimdi yeniden başlatmak istiyor musunuz?", L"Yeniden başlatmak gerekir", MB_YESNOCANCEL);
+    if (response == IDYES || response == IDNO ||response == IDCANCEL)
+        std::cout << "Virtual machine not detected.\n";
+    else
+        std::cout << "Virtual machine detected.\n";
+
+        
+
+   
+
+    return 0;
+}
+
---
+++ Check-CPU-RAM.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+int main()
+{
+    // Tipik bir analistçinin analist yaptığı sanal makinede 2 çekirdekli bir işlemci ve 2 GB RAM bulunduğunu düşünürsek ;
+
+    // Check CPU.
+    SYSTEM_INFO lpSystemInfo; // SYSTEM_INFO struct'dan bir pointer tanımladık. Bu yapı mevcut olan bilgisayar sistemi hakkında bilgileri içerir.
+    GetSystemInfo(&lpSystemInfo); // GetSystemInfo API'ı tek parametre alıyor. O da SYSTEM_INFO yapısından (structure) bir pointer.
+    DWORD numberOfProcessors = lpSystemInfo.dwNumberOfProcessors; // Çekirdek sayısını değişkenimize atadık.
+    if (numberOfProcessors < 2) // Eğer ikiden küçükse false döndürüyoruz.
+        std::cout << "Virtual machine detected !\n";
+
+    // Check RAM
+    MEMORYSTATUSEX memoryStatus; // MEMORYSTATUSEX yapısından bir pointer tanımladık. Bu yapı genişletilmiş bellek dahil sanal ve fiziksel belleklerin mevcut durumunu içerir.
+    memoryStatus.dwLength = sizeof(memoryStatus); // Byte cinsinden boyutunu, MEMORYSTATUSEX'den tanımladığımız değişkene atıyoruz.
+    GlobalMemoryStatusEx(&memoryStatus); // GlobalMemoryStatusEx API'ı tek parametre alıyor. O da MEMORYSTATHSEX yapısından bir pointer.
+    DWORD RAMMB = memoryStatus.ullTotalPhys / 1024 / 1024; // Byte cinsinden fiziksel memorynin miktarını alıyoruz. Byte cinsinden aldığımız için bölüyoruz.
+    if (RAMMB < 2048) // Eğer 2GB'dan küçükse false döndürüyoruz.
+        std::cout << "Virtual machine detected ! (RAM)\n";
+
+    return 0;
+}
+
